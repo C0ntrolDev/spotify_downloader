9ab01c55a49f2aac4e675f1e8106c0330f9df6a9
Roman Deviaterikov <erdcontroldev@gmail.com>
2024-01-07 12:26:17 +1000

    добавил локализацию для уведомления о загрузке в фоне

--- lib/core/background/background.dart
+++ lib/core/background/background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_background/flutter_background.dart';
+import 'package:spotify_downloader/generated/l10n.dart';
 
 Future<void> initBackground() async {
   bool success = await _tryInitBackground();
@@ -8,11 +9,11 @@ Future<void> initBackground() async {
 }
 
 Future<bool> _tryInitBackground() {
-   const androidConfig = FlutterBackgroundAndroidConfig(
-    notificationTitle: "You can close the app and the download will continue !",
-    notificationText: "(you can delete this message)",
+  final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: S().youCanCloseTheAppAndTheDownloadWillContinue,
+    notificationText: S().youCanDeleteThisMessage,
     notificationImportance: AndroidNotificationImportance.Default,
-    notificationIcon: AndroidResource(name: 'notifications_icon', defType: 'drawable'),
+    notificationIcon: const AndroidResource(name: 'notifications_icon', defType: 'drawable'),
   );
 
   return FlutterBackground.initialize(androidConfig: androidConfig);
